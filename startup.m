6d347d22475ee07f76a299bf96f541ae13c34cc1
Daniel Hull <danielhull13@gmail.com>
2014-09-12 08:56:37 -0400

    created startup.m
    This file will help anyone add folders and packages to the pathway

---
+++ startup.m
@@ -0,0 +1,10 @@
+GIT_ROOT = '/Users/danielhull13/repos';
+addpath(fullfile(GIT_ROOT,'matlab_standard_library'));
+addpath(fullfile(GIT_ROOT,'matlab_grille_library_danielhull'));
+addpath(fullfile(GIT_ROOT,'adinstruments_sdk_matlab'));
+addpath(fullfile(GIT_ROOT,'bladder_analysis'));
+
+SCHOOL_ROOT = '/Users/danielhull13/Documents/MATLAB';
+
+addpath(fullfile(SCHOOL_ROOT, 'Grill Lab'));
+addpath(fullfile(SCHOOL_ROOT, 'ECE'));
