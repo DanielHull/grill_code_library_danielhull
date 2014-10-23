6f828902bb70487a2ee278c4b9992b4dda811ec0
DanielHull <danielhull13@gmail.com>
2014-10-23 14:45:50 -0400

    update startup

--- startup.m
+++ startup.m
@@ -3,6 +3,7 @@ addpath(fullfile(GIT_ROOT,'matlab_standard_library'));
 addpath(fullfile(GIT_ROOT,'matlab_grille_library_danielhull'));
 addpath(fullfile(GIT_ROOT,'adinstruments_sdk_matlab'));
 addpath(fullfile(GIT_ROOT,'bladder_analysis'));
+addpath(fullfile(GIT_ROOT, 'rnel_matlab'));
 
 SCHOOL_ROOT = '/Users/danielhull13/Documents/MATLAB';
 
